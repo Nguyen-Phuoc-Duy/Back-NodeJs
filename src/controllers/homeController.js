6bc0a664b64f792a2d1e27fb17ee735d1dc023db
PhuocDuy <nguyenphuocduy2908@gmail.com>
2023-05-30 15:01:28 +0700

    edit-deleteCRUD

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -23,11 +23,39 @@ let postCRUD = async (req, res) => {
 };
 let displayGetCRUD = async (req, res) => {
   let data = await CRUDService.getAllUser();
-  console.log("================");
-  console.log(data);
-  console.log("================");
+  // console.log("================");
+  // console.log(data);
+  // console.log("================");
   // return res.send("get crud display");
   return res.render("displayCRUD.ejs", { dataTable: data });
+};
+let getEditCRUD = async (req, res) => {
+  console.log(req.query.id);
+  let userId = req.query.id;
+  if (userId) {
+    let userData = await CRUDService.getUserInfoById(userId);
+    console.log("????????????", userData);
+    return res.render("editCRUD.ejs", {
+      user: userData,
+    });
+  } else {
+    return res.send("user not found");
+  }
+};
+let putCRUD = async (req, res) => {
+  let data = req.body;
+  await CRUDService.updateUserData(data);
+  // return res.send("update done");
+  return res.redirect("/get-crud");
+};
+let deleteCRUD = async (req, res) => {
+  let id = req.query.id;
+  if (id) {
+    await CRUDService.deleteUserById(id);
+    return res.send("delete done");
+  } else {
+    return res.send("delete failed");
+  }
 };
 module.exports = {
   getHomePage: getHomePage,
@@ -35,4 +63,7 @@ module.exports = {
   getCRUD: getCRUD,
   postCRUD: postCRUD,
   displayGetCRUD: displayGetCRUD,
+  getEditCRUD: getEditCRUD,
+  putCRUD: putCRUD,
+  deleteCRUD: deleteCRUD,
 };
